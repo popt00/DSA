b688cbecb71f9e00bfea2776468b4a8c3ce9ed4b
Parimal Donga <47852029+popt00@users.noreply.github.com>
2024-07-27 07:45:59 -0400

    Update minimumCostToConvertStringII.java

--- leetcode/minimumCostToConvertStringII.java
+++ leetcode/minimumCostToConvertStringII.java
@@ -3,56 +3,70 @@
 */
 class Solution {
     int len=0;
-
     public long minimumCost(String source, String target, String[] original, String[] changed, int[] cost) {
         Integer index=0;
-        Trie strie=new Trie();
+        Trie root=new Trie();
+        //adding all string to get index (like adding to arraylist)
         for(int i=0;i<original.length;i++){
-            strie.get(original[i]);strie.get(changed[i]);
+            root.get(original[i]);root.get(changed[i]);
         }
+        //adjacent matrix for original to target
         int n=len;
-        int[][] dp=new int[n][n];
-        for(int i=0;i<n;i++){Arrays.fill(dp[i],Integer.MAX_VALUE);dp[i][i]=0;}
+        int[][] adj=new int[n][n];
+        for(int i=0;i<n;i++){Arrays.fill(adj[i],Integer.MAX_VALUE);adj[i][i]=0;}
         for(int i=0;i<cost.length;i++){
-            int oriIndex=strie.get(original[i]), chaIndex=strie.get(changed[i]);
-            dp[oriIndex][chaIndex]=Math.min(dp[oriIndex][chaIndex],cost[i]);
+            int oriIndex=root.get(original[i]), chaIndex=root.get(changed[i]);
+            adj[oriIndex][chaIndex]=Math.min(adj[oriIndex][chaIndex],cost[i]);
         }
+        //as a->b, b->c is there then a->c will be there so doing floyd warshall algorithm
+        floydWarshall(adj);
+        return helper(source.toCharArray(),target.toCharArray(),adj,root);
+    }
+
+    public long helper(char[] s, char[] t, int[][] adj,Trie root){
+        int m=s.length;
+        long[] dp=new long[m+1];
+        for(int i=0;i<=m;i++)dp[i]=Long.MAX_VALUE;
+        dp[0]=0;
+        
+        for(int i=0;i<m;i++){
+            if(dp[i]==Long.MAX_VALUE)continue;
+            if(s[i]==t[i])dp[i+1]=Math.min(dp[i+1],dp[i]);
+
+            //as we are searching from root everytime
+            Trie souTrie=root,tarTrie=root;
+            inner: for(int j=i;j<m;j++){
+                //if we get index from source and target we can find adj[i][j] of that
+                souTrie=souTrie.child[(int)(s[j]-'a')];
+                tarTrie=tarTrie.child[(int)(t[j]-'a')];
+                //if we do not have any souce or target then there is no point in going further
+                if(souTrie==null || tarTrie == null){break inner;}
+                if(souTrie.val!=-1 && tarTrie.val!=-1 && adj[souTrie.val][tarTrie.val] != Integer.MAX_VALUE){
+                    dp[j+1]=Math.min(dp[j+1],adj[souTrie.val][tarTrie.val]+dp[i]);
+                }
+            }
+        }
+        return dp[m]==Long.MAX_VALUE?-1:dp[m];
+    }
+    public void floydWarshall(int [][] adj){
+        int n=adj.length;
         for(int k=0;k<n;k++){
             for(int i=0;i<n;i++){
-                if(dp[i][k]!=Integer.MAX_VALUE)
+                if(adj[i][k]!=Integer.MAX_VALUE)
                     for(int j=0;j<n;j++){
-                        if(dp[k][j]!=Integer.MAX_VALUE && dp[i][j]>dp[i][k]+dp[k][j]){
-                            dp[i][j]=dp[i][k]+dp[k][j];
+                        if(adj[k][j]!=Integer.MAX_VALUE && adj[i][j]>adj[i][k]+adj[k][j]){
+                            adj[i][j]=adj[i][k]+adj[k][j];
                         }
                     }
             }
         }
-        
-        char[] s=source.toCharArray();char[] t=target.toCharArray();
-        int m=source.length();
-        long[] ans=new long[m+1];
-        for(int i=0;i<=m;i++)ans[i]=Long.MAX_VALUE;
-        ans[0]=0;
-        for(int i=0;i<m;i++){
-            if(ans[i]==Long.MAX_VALUE)continue;
-            Trie souTrie=strie,tarTrie=strie;
-            inner: for(int j=i;j<m;j++){
-                souTrie=souTrie.child[(int)(s[j]-'a')];
-                tarTrie=tarTrie.child[(int)(t[j]-'a')];
-                if(souTrie==null || tarTrie == null){break inner;}
-                if(souTrie.val!=-1 && tarTrie.val!=-1){
-                    ans[j+1]=Math.min(ans[j+1],dp[souTrie.val][tarTrie.val]+ans[i]);
-                }
-            }
-        }
-        return ans[m]==Long.MAX_VALUE?-1:ans[m];
     }
 
     class Trie{
         int val;
         Trie[] child;
         Trie(){child=new Trie[26];val=-1;}
-        int get(String str){
+        int get(String str){//serves as both add or get str
             char[]chstr =str.toCharArray();
             Trie temp=this;
             for(int i=0;i<chstr.length;i++){
