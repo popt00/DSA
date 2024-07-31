18c71f3961538942831cde32968ec095f4ad6dd7
Parimal Donga <47852029+popt00@users.noreply.github.com>
2024-07-31 04:20:30 -0400

    Update C.java
    accepted

--- codeforces/1995/C.java
+++ codeforces/1995/C.java
@@ -2,7 +2,7 @@
  * https://codeforces.com/contest/1995/problem/C
  * uncomplete: wrong answer on test c
  * */
-package org.donga;
+// package org.donga;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Scanner;
@@ -14,39 +14,37 @@ public class C{
         while(t-->0){
             int n=s.nextInt();
             long m=s.nextLong();
-            int [][] p=new int[n][2];
+            long [][] p=new long[n][2];
             long ans=0;
             for(int i=0;i<n;i++){
-                p[i][0]=s.nextInt();
+                p[i][0]=s.nextLong();
             }
             for(int i=0;i<n;i++){
-                p[i][1]=s.nextInt();
-                p[i][1]=Math.min(p[i][1],(int)(m/p[i][0]));
-                ans=Math.max(ans,((long)p[i][1])*p[i][0]);
+                p[i][1]=s.nextLong();
             }
-            Arrays.sort(p,new Comparator<int[]>() {
-                public int compare(int[] o1, int[] o2) {
-                    return o1[0]-o2[0];
+            Arrays.sort(p,new Comparator<long[]>() {
+                public int compare(long[] o1, long[] o2) {
+                    return Long.compare(o1[0],o2[0]);
                 }
             });
             for(int i=0;i+1<n;i++){
+                ans=Math.max(ans,Math.min(p[i][1],m/p[i][0])*p[i][0]);
                 if(p[i][0]+1==p[i+1][0]){
                     ans=Math.max(ans,helper(p[i],p[i+1],m));
                 }
             }
+            ans=Math.max(ans,Math.min(p[n-1][1],m/p[n-1][0])*p[n-1][0]);
             System.out.println(ans);
         }
     }
-    public static long helper(int []a, int []b, long m){
-        long p1=1l*a[0]*a[1];
-        long p2=1l*b[0]*b[1];
-        if(p1+p2<=m)return p1+p2;
-        long coins= m-p1;
-        long b_count= coins/b[0];
+    public static long helper(long []a, long []b, long m){
+        long a_count=Math.min(a[1],m/a[0]);
+        long coins= m-a[0]*a_count;
+        long b_count= Math.min(b[1],coins/b[0]);
         coins-=b_count*b[0];
-        long exchangeAB= Math.min(Math.min(b[1]-b_count,a[1]),coins);
-        long max=((long)a[1]-exchangeAB)*a[0]+(b_count+exchangeAB)*b[0];
-        //System.out.println(a[0]+"*"+(a[1]-exchangeAB)+"+"+b[0]+"*"+(b_count+exchangeAB)+"="+max+" :["+a[1]+","+b[1]+"].");
-        return max;
+        long exchangeAB= Math.min(Math.min(b[1]-b_count,a_count),coins);
+        a_count-=exchangeAB;
+        b_count+=exchangeAB;
+        return a_count*a[0]+b_count*b[0];
     }
 }
