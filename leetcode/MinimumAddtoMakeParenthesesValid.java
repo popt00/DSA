880df23000dadacf1854b23c0505654529a48d7d
Parimal Donga <47852029+popt00@users.noreply.github.com>
2024-10-11 03:17:39 -0400

    Create MinimumAddtoMakeParenthesesValid.java

---
+++ leetcode/MinimumAddtoMakeParenthesesValid.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int count=0,ans=0;
+        for(char c: s.toCharArray()){
+            if(c=='(')
+                count++;
+            else {
+                count--;
+                if(count<0){
+                    count=0;
+                    ans++;
+                }
+            }
+        }
+        return ans+count;
+    }
+}
