50b8c1672bc3e0e9a551d4cf37a4df3d925116d0
Parimal Donga <47852029+popt00@users.noreply.github.com>
2024-10-30 03:45:35 -0400

    Update HeightofBinaryTreeAfterSubtreeRemovalQueries.java

--- leetcode/HeightofBinaryTreeAfterSubtreeRemovalQueries.java
+++ leetcode/HeightofBinaryTreeAfterSubtreeRemovalQueries.java
@@ -1,66 +1,56 @@
-//https://leetcode.com/problems/height-of-binary-tree-after-subtree-removal-queries/
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+//https://leetcode.com/problems/height-of-binary-tree-after-subtree-removal-queries
 class Solution {
-    HashMap<Integer,Integer> heightMap;
+    //  heightOfTree;
+    // ArrayList<ArrayList<Integer>> levelAr;
+    ArrayList<Max2> levelAr;
+    HashMap<Integer,Integer> mapLevel;
+    class Max2{
+        int val;
+        int hei;
+        int secondhei;
+        Max2(int val,int hei){this.val=val;this.hei=hei;}
+        void add(int val,int heiadd){
+            if(this.hei<heiadd){
+                this.secondhei=this.hei;
+                this.hei=heiadd;
+                this.val=val;
+            }
+            else if(this.secondhei<heiadd){
+                this.secondhei=heiadd;
+            }
+        }
+    }
     public int[] treeQueries(TreeNode root, int[] queries) {
-        heightMap= new HashMap<>();
-        int n = getNumberOfNodes(root);
-        int[] index=new int[1];index[0]=1;
-        int[] swapped=new int[n+1];
-        setSwap(root, swapped, index);
-        setMapHeight(root);
-        int[] ans= new int[queries.length];
+        int n = getTotalNodesNumber(root);
+        levelAr=new ArrayList<>();
+        mapLevel= new HashMap<>();
+        int heightOfTree = fillHeights(root,0)-1;
+        int[] ans=new int[queries.length];
         for(int i=0;i<queries.length;i++){
-            ans[i]=getHeightAfterRemoval(root,swapped[queries[i]])-1;
+            int q=queries[i];
+            int level= mapLevel.get(q);
+            if(levelAr.get(level).val==q){
+                ans[i]=level+levelAr.get(level).secondhei-1;
+            }
+            else ans[i]=heightOfTree;
+            
         }
-        return ans;
+       return ans;
     }
-    public int getNumberOfNodes(TreeNode root){
+    public int getTotalNodesNumber(TreeNode root){
         if(root==null)return 0;
-        return 1 + getNumberOfNodes(root.left)+getNumberOfNodes(root.right);
+        return 1 + getTotalNodesNumber(root.left) + getTotalNodesNumber(root.right);
     }
-    public void setSwap(TreeNode root, int[] swap, int[] index){
-        if(root==null)return;
-        setSwap(root.left,swap,index);
-        swap[root.val]=index[0];
-        root.val=index[0]++;
-        setSwap(root.right,swap,index);
-    }
-    //sets map height
-    public int setMapHeight(TreeNode root){
+    public int fillHeights(TreeNode root, int level){
         if(root==null)return 0;
-        int right=setMapHeight(root.right);
-        int left=setMapHeight(root.left);
-        int height = 1+Math.max(right,left);
-        heightMap.put(root.val,height);
+        if(levelAr.size()<=level)levelAr.add(new Max2(0,0));
+        int height = 1 + Math.max(
+                                    fillHeights(root.left, level+1),
+                                    fillHeights(root.right, level+1)
+                                );
+        
+        levelAr.get(level).add(root.val,height);
+        mapLevel.put(root.val,level);
         return height;
-    }
-    
-    public int getHeightAfterRemoval(TreeNode root, int val){
-        if(root==null || root.val==val)return 0;
-        int left=0,right=0;
-        if(root.val<val){
-            if(root.left!=null)left=heightMap.get(root.left.val);
-            right=getHeightAfterRemoval(root.right,val);
-        }
-        else {
-            if(root.right!=null)right=heightMap.get(root.right.val);
-            left=getHeightAfterRemoval(root.left,val);
-        }
-        return Math.max(left,right)+1;
     }
 }
