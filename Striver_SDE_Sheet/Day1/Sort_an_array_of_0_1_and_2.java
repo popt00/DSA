58071984bd263f216c74370f5d4466b1b44758d7
Parimal Donga <47852029+popt00@users.noreply.github.com>
2025-02-28 03:46:49 -0500

    Update Sort_an_array_of_0_1_and_2.java

--- Striver_SDE_Sheet/Day1/Sort_an_array_of_0_1_and_2.java
+++ Striver_SDE_Sheet/Day1/Sort_an_array_of_0_1_and_2.java
@@ -1,1 +1,21 @@
-
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero=0,j=nums.length-1;
+        for(int i=0;i<=j;){
+            if(nums[i]==1){
+                i++;
+            }
+            else if(nums[i]==0){
+                nums[i]=nums[zero];
+                nums[zero]=0;
+                zero++;
+                i++;
+            }
+            else if(nums[i]==2){
+                nums[i]=nums[j];
+                nums[j]=2;
+                j--;
+            }
+        }
+    }
+}
