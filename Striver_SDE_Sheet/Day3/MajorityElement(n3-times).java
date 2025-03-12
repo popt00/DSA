3f0c9b2b5824a532e5d2d148617f57b17fc7e240
Parimal D <47852029+popt00@users.noreply.github.com>
2025-03-12 04:39:57 -0400

    Update MajorityElement(n3-times).java

--- Striver_SDE_Sheet/Day3/MajorityElement(n3-times).java
+++ Striver_SDE_Sheet/Day3/MajorityElement(n3-times).java
@@ -1,18 +1,33 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int maj1=0,maj2=0;
-        int ele1=0,ele2=0;
+        int ele1=0,ele2=1;
         for(int x:nums){
-            if(maj1==0){maj1++; ele1=x;}
-            else if(maj2==0){maj2++; ele2=x;}
-            else if(ele1==x)maj1++;
+            if(ele1==x)maj1++;
             else if(ele2==x)maj2++;
-            else {maj1--;maj2--;}
+            else if(maj1==0){
+                maj1=1;ele1=x;
+            }
+            else if(maj2==0){
+                maj2=1;ele2=x;
+            }
+            else{
+                maj1--;maj2--;
+            }
         }
+        maj1=count(nums,ele1);
+        maj2=count(nums,ele2);
         int comparatorValue=nums.length/3;
-        if(maj1>comparatorValue && maj2>comparatorValue)return Arrays.asList(ele1,ele2);
+        if(maj1>comparatorValue && maj2>comparatorValue && ele1!=ele2)return Arrays.asList(ele1,ele2);
         if(maj1>comparatorValue)return Arrays.asList(ele1);
         if(maj2>comparatorValue)return Arrays.asList(ele2);
         return Arrays.asList();
+    }
+    public int count(int[] arr, int n){
+        int count=0;
+        for(int x: arr){
+            if(x==n)count++;
+        }
+        return count;
     }
 }
