12cb91af6e3266aa0e470a242c4cb93369086b6b
Parimal D <47852029+popt00@users.noreply.github.com>
2025-03-20 03:43:38 -0400

    Create Detect-a-cycle-in-Linked-List.java

---
+++ Striver_SDE_Sheet/Day6/Detect-a-cycle-in-Linked-List.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null)return false;
+        ListNode slowPointer=head,fastPointer=head;
+        do{
+            slowPointer=slowPointer.next;
+            fastPointer=fastPointer.next;
+            if(fastPointer!=null)fastPointer=fastPointer.next;
+        }while(fastPointer!=null && slowPointer!=fastPointer);
+        
+        return fastPointer!=null;
+    }
+}
