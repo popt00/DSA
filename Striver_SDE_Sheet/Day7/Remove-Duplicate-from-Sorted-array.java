58810025da785f434ec630b7901ffb5ea9c9e63e
Parimal D <47852029+popt00@users.noreply.github.com>
2025-03-24 15:56:55 -0400

    Create Remove-Duplicate-from-Sorted-array.java

---
+++ Striver_SDE_Sheet/Day7/Remove-Duplicate-from-Sorted-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=0,j=0,n=nums.length;
+        while(j<n){
+            nums[i]=nums[j];
+            while(j<n && nums[j]==nums[i])j++;
+            i++;
+        }
+        return i;
+    }
+}
